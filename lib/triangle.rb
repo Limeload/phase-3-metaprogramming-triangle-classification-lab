3fd4d6cf616471d80cb2de5454fd0b63eb627717
Shraddha Rao <shraddharaom@gmail.com>
2023-01-12 07:19:05 -0600

    commit tree 603efa02d92d24964acfb246d434f13a65afeb7c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
-class Triangle
-  # write code here
+# require 'pry'
+class Triangle < StandardError
+  
+  attr_reader :a, :b , :c
+
+  def initialize(a, b, c)
+   @a = a
+   @b = b
+   @c = c
+  end
+
+  def kind 
+   triangle_error_message
+   if self.a == self.b && self.b == self.c
+    :equilateral
+   elsif self.a == self.b || self.b == self.c || self.c == self.a
+    :isosceles
+   else
+    :scalene
+  end
+end
+
+def triangle_error_message
+  raise TriangleError unless [self.a, self.b, self.c].all?(&:positive?) && 
+  self.a + self.b > self.c && self.a + self.c > self.b && self.b + self.c > self.a
+end
+
+class TriangleError < StandardError
+  # triangle error code
+end
+
 end
